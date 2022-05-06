f727d3ec2772e4e6e17ee44a8b8513f329ead6cb
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2022-05-06 14:30:04 +0200

    Delete publish.cmd

--- medical-bot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=devislandmedicalbot-Web-Deploy.pubxml -p:Password=JnYcu156xtlj3qWG0DidD9v7amxNoyLtFyxm8ApDRuHJqxYsKjMjXl2sS3he
-
